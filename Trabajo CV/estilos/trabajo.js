53314dd0048bb89f23144e88145ada7199acd5e4
gabri <gabriel_cen@hotmail.com>
2023-01-29 18:05:18 -0300

    Primer repositorio

---
+++ Trabajo CV/estilos/trabajo.js
@@ -0,0 +1,8 @@
+function primerboton() {
+
+    document.getElementById('grancon').setAttribute('style', 'display: grid');
+    document.getElementById('primero').setAttribute('style', 'display: none');
+    document.getElementById('social').setAttribute('style', 'display: grid');
+
+}
+
