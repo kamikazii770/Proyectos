f7e9bf01342e2a87bd0c8ebafeb9585e00b0ccf2
gabri <gabriel_cen@hotmail.com>
2023-01-30 03:39:57 -0300

    proyectoweb

---
+++ proyecto_final/estilos/js.js
@@ -0,0 +1,7 @@
+function primerboton() {
+
+    document.getElementById('grancon').setAttribute('style', 'display: grid');
+    document.getElementById('primero').setAttribute('style', 'display: none');
+    document.getElementById('reprod').setAttribute('style', 'display: grid');
+
+}
