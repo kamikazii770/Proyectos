0c3084536d6bc368af42cb2784d0fc5d9971403f
gabri <gabriel_cen@hotmail.com>
2023-01-30 07:00:11 -0300

    proyectofinalizado

--- proyecto_final/estilos/js.js
+++ proyecto_final/estilos/js.js
@@ -1,7 +1,10 @@
+
 function primerboton() {
+        document.getElementById('grancon').setAttribute('style', 'display: grid');
+        document.getElementById('primero').setAttribute('style', 'display: none');
+        document.getElementById('reprod').setAttribute('style', 'display: grid');
+}
 
-    document.getElementById('grancon').setAttribute('style', 'display: grid');
-    document.getElementById('primero').setAttribute('style', 'display: none');
-    document.getElementById('reprod').setAttribute('style', 'display: grid');
-
+function dargrax(){
+window.alert('Muchas gracias por contactarse');
 }
