9f92b813e3788733fe66ab165a239ff5726e5b28
gabri <gabriel_cen@hotmail.com>
2023-01-30 01:02:09 -0300

    ultimos cambios

--- Trabajo CV/estilos/trabajo.js
+++ Trabajo CV/estilos/trabajo.js
@@ -2,7 +2,7 @@ function primerboton() {
 
     document.getElementById('grancon').setAttribute('style', 'display: grid');
     document.getElementById('primero').setAttribute('style', 'display: none');
-    document.getElementById('social').setAttribute('style', 'display: grid');
+    document.getElementById('reprod').setAttribute('style', 'display: grid');
 
 }
 
